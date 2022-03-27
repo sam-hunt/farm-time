c61e284756045a92588622d493797f158b0fc75c
Sam Hunt <samhunt.dev@gmail.com>
2022-03-27 14:02:10 +1300

    refactor: improve Nav code density for readability

--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -13,12 +13,8 @@ import { ThemeContext } from '../App/App';
 const Nav = () => {
     const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
     const open = Boolean(anchorEl);
-    const handleClick: MouseEventHandler<HTMLButtonElement> = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+    const handleClick: MouseEventHandler<HTMLButtonElement> = (event) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
 
     const { currentTheme, toggleTheme } = useContext(ThemeContext);
 
@@ -32,59 +28,32 @@ const Nav = () => {
                         aria-haspopup='true'
                         aria-expanded={open ? 'true' : undefined}
                         onClick={handleClick}
-
                         size='large'
                         edge='start'
                         color='inherit'
                         aria-label='menu'
                         sx={{ mr: 2 }}
                     >
-                        <Icon path={mdiMenu}
-                            title='Menu'
-                            size={1}
-                        />
+                        <Icon path={mdiMenu} title='Menu' size={1}/>
                     </IconButton>
                     <Menu
                         id='nav-menu'
                         anchorEl={anchorEl}
                         open={open}
                         onClose={handleClose}
-                        MenuListProps={{
-                            'aria-labelledby': 'menu-button',
-                        }}
+                        MenuListProps={{ 'aria-labelledby': 'menu-button' }}
                     >
                         <MenuItem component={Link} to={'/calendar'} onClick={handleClose}>
-                            <ListItemIcon>
-                                <Icon path={mdiCalendar}
-                                    title='Menu'
-                                    size={1}
-                                />
-                            </ListItemIcon>
-                            <ListItemText>
-                                Calendar
-                            </ListItemText>
+                            <ListItemIcon><Icon path={mdiCalendar} title='Menu' size={1}/></ListItemIcon>
+                            <ListItemText>Calendar</ListItemText>
                         </MenuItem>
                         <MenuItem component={Link} to={'/reports'} onClick={handleClose}>
-                            <ListItemIcon>
-                                <Icon path={mdiSigma}
-                                    title='Menu'
-                                    size={1}
-                                />
-                            </ListItemIcon>
-                            <ListItemText>
-                                Reports
-                            </ListItemText>
+                            <ListItemIcon><Icon path={mdiSigma} title='Menu' size={1}/></ListItemIcon>
+                            <ListItemText>Reports</ListItemText>
                         </MenuItem>
                         <MenuItem component={Link} to={'/backup-restore'} onClick={handleClose}>
-                            <ListItemIcon>
-                                <Icon path={mdiBackupRestore}
-                                    title='Menu'
-                                    size={1}
-                                />
-                            </ListItemIcon>
-                            <ListItemText>
-                                Backup/Restore
-                            </ListItemText>
+                            <ListItemIcon><Icon path={mdiBackupRestore} title='Menu' size={1}/></ListItemIcon>
+                            <ListItemText>Backup/Restore</ListItemText>
                         </MenuItem>
                     </Menu>
                     <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
